27032d5d9af172b3e3ea124a933c387e506a9cd6
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-03-28 22:24:57 +0100

    updated in app

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChartContainer from './components/chart-container';
+import SearchFilter from './components/search-filter';
+import teamData from './mocks/team-data.json';
+import { fetchResults } from './utilities/mock-fn';
+import { debounce } from 'lodash';
 
 import './reset.scss';
 
-const App = () => (
-  <ChartContainer />
-);
+const results = teamData.map((item) => {
+  const team = item.team_name.split(' ');
+  const suggestion = teamData.find((name) => team.indexOf(name) !== -1);
+
+  return {
+    ...item,
+    team,
+    suggestion,
+  };
+});
+
+const App = () => {
+  const [fetchResultsRequestStatus, setRequest] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleFetchResultsSuccess = (payload) => {
+    setRequest('success');
+    setSearchResult(payload);
+  };
+
+  const handleFetchResultsRejected = () => {
+    setRequest('rejected');
+  };
+
+  const fetchSearchResults = (searchValue) => {
+    setRequest('pending');
+
+    fetchResults(searchValue)
+      .then(handleFetchResultsSuccess)
+      .catch(handleFetchResultsRejected);
+  };
+
+  const handleSearchFilterChange = debounce((searchValue) => {
+    fetchSearchResults(searchValue);
+  }, 200);
+
+  // const handleSearchFilterChange = (searchValue) => {
+  //   fetchSearchResults(searchValue);
+  // };
+
+  return (
+    <>
+      <SearchFilter
+        placeholder="player's name"
+        fullWord={results[0].suggestion || ''}
+        onChange={handleSearchFilterChange}
+      />
+
+      {fetchResultsRequestStatus === 'pending' && (
+        <div>Loading...</div>
+      )}
+
+      {fetchResultsRequestStatus === 'rejected' && (
+        <div>We cannot reach the server, please try again</div>
+      )}
+
+      {fetchResultsRequestStatus === 'success' && !!searchResult.length && (
+        <ChartContainer searchResult={searchResult} />
+      )}
+    </>
+  );
+};
 
 export default App;

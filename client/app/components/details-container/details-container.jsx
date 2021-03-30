9f5e71aa1bd520be8272185aa7bed18990381384
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-03-30 20:51:04 +0100

    styles for details container

--- client/app/components/details-container/details-container.jsx
+++ client/app/components/details-container/details-container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Text from '../text';
 import { Row, Column } from '../grid';
+import styles from './details-container.scss';
 
 const DetailsContainer = ({ searchResult, playerData, teamData }) => {
   const playerId = searchResult.player_id;
@@ -14,34 +15,51 @@ const DetailsContainer = ({ searchResult, playerData, teamData }) => {
     backgroundColor: `${team_first_color}`,
     width: '40px',
     height: '15px',
+    marginLeft: '5px',
   };
 
   return (
-  <div>
-    <Text size="medium">{player_name}</Text>
-    <Row>
-      <Column>
-        <Text size="small">Country: {country_name}</Text>
-        <Text size="small">Date of birth: {player_birth_date}</Text>
-      </Column>
-      <Column>
-        <Text size="small">Team: {team_name}</Text>
-        <Row>
-          <Column>
-            <Text size="small">Colour:</Text>
-          </Column>
-          <Column>
-            <div style={style} />
-          </Column>
-        </Row>
-      </Column>
-      <Column>
-        <Text size="small">Minutes played: {searchResult.minutes_played}</Text>
-        <Text size="small">Goals: {searchResult.goals}</Text>
-      </Column>
-    </Row>
+    <div className={styles['details-container']}>
+      <Text size="medium" center>{player_name}</Text>
+      <Row justifyContent="space-between">
+        <Column>
+          <Text size="small">
+            <b>Country: </b>
+            {country_name}
+          </Text>
+          <Text size="small">
+            <b>Date of birth: </b>
+            {player_birth_date}
+          </Text>
+        </Column>
+        <Column>
+          <Text size="small">
+            <b>Team: </b>
+            {team_name}
+          </Text>
+          <Row>
+            <Column>
+              <Text size="small"><b>Colour: </b></Text>
+            </Column>
+            <Column>
+              <div style={style} />
+            </Column>
+          </Row>
+        </Column>
+        <Column>
+          <Text size="small">
+            <b>Minutes played: </b>
+            {searchResult.minutes_played}
+          </Text>
+          <Text size="small">
+            <b>Goals: </b>
+            {searchResult.goals}
+          </Text>
+        </Column>
+      </Row>
 
-  </div>
-)};
+    </div>
+  );
+};
 
 export default DetailsContainer;

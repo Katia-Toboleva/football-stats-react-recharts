ed30b2a252ba02b1b7d707643f809a0af4286306
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-03-30 01:02:19 +0100

    updated filters

--- client/app/components/filters/filters.jsx
+++ client/app/components/filters/filters.jsx
@@ -1,119 +1,14 @@
 import React, { useState } from 'react';
 import { Row, Column } from '../grid';
-import teamData from '../../mocks/team-data.json';
-import playerData from '../../mocks/player-data.json';
-import matchData from '../../mocks/match-data.json';
-import statData from '../../mocks/stat-data.json';
+import { getPlayersData, getTeamsData, getMatchesData } from '../../utilities/filters-utilities';
 
-const getPlayersData = (teamId, matchId) => {
-  if (teamId && !matchId) {
-    const playerPerTeamArr = statData.filter(item => item.team_id === teamId);
-    const playerPerTeamIds = playerPerTeamArr.map(item => item.player_id);
-    const playersPerTeamData = playerPerTeamIds.map(id => playerData.find(item => item.player_id === id));
-
-    return playersPerTeamData;
-  }
-
-  if (!teamId && matchId) {
-    const playerPerMatchArr = statData.filter(item => item.match_id === matchId);
-    const playerPerMatchIds = playerPerMatchArr.map(item => item.player_id);
-    const playersPerMatchData = playerPerMatchIds.map(id => playerData.find(item => item.player_id === id));
-
-    return playersPerMatchData;
-  }
-
-  if (teamId && matchId) {
-    const playerPerTeamAndMatchArr = statData.filter(item => item.team_id === teamId && item.match_id === matchId);
-    const playerPerTeamAndMatchIds = playerPerTeamAndMatchArr.map(item => item.player_id);
-    const playersPerTeamAndMatchData = playerPerTeamAndMatchIds.map(id => playerData.find(item => item.player_id === id));
-
-    return playersPerTeamAndMatchData;
-  }
-
-  return playerData;
-};
-
-const getTeamsData = (playerId, matchId) => {
-  if (playerId && !matchId) {
-    const teamPerPlayerArr = statData.filter(item => item.player_id === playerId);
-
-    if (!teamPerPlayerArr.length) {
-      return [];
-    }
-
-    const teamPerPlayerId = teamPerPlayerArr[0].team_id;
-    const teamPerPlayerData = teamData.find(item => item.team_id === teamPerPlayerId);
-    console.log([teamPerPlayerData]);
-
-    return [teamPerPlayerData];
-  }
-
-  if (!playerId && matchId) {
-    const teamPerMatchArr = statData.filter(item => item.match_id === matchId);
-    const teamPerMatchIds = teamPerMatchArr.map(item => item.team_id);
-    const reducedTeamPerMatchIds = teamPerMatchIds.reduce((acc, currentValue) => (acc.includes(currentValue) ? acc : [...acc, currentValue]), []);
-    const teamPerMatchArrData = reducedTeamPerMatchIds.map(id => teamData.find(item => item.team_id === id));
-
-    return teamPerMatchArrData;
-  }
-
-  if (playerId && matchId) {
-    const teamPerMatchAndPlayerArr = statData.filter(item => item.player_id === playerId && item.match_id === matchId);
-
-    if (!teamPerMatchAndPlayerArr.length) {
-      return [];
-    }
-
-    const teamPerMatchAndPlayerId = teamPerMatchAndPlayerArr[0].team_id;
-    const teamPerMatchAndPlayerData = teamData.find(item => item.team_id === teamPerMatchAndPlayerId);;
-
-    return [teamPerMatchAndPlayerData];
-  }
-
-  return teamData;
-};
-
-const getMatchesData = (teamId, playerId) => {
-  if (playerId && !teamId) {
-    const matchPerPlayerArr = statData.filter(item => item.player_id === playerId);
-
-    if (!matchPerPlayerArr.length) {
-      return [];
-    }
-
-    const matchPerPlayerIds = matchPerPlayerArr.map(item => item.match_id);
-    const matchPerPlayerData = matchPerPlayerIds.map(id => matchData.find(item => item.match_id === id));
-
-    return matchPerPlayerData;
-  }
-
-  if (!playerId && teamId) {
-    const matchPerTeamArr = statData.filter(item => item.team_id === teamId);
-    const matchPerTeamIds = matchPerTeamArr.map(item => item.match_id);
-    const reducedMatchPerTeamArrData = matchPerTeamIds.reduce((acc, currentValue) => (acc.includes(currentValue) ? acc : [...acc, currentValue]), []);
-
-    const matchPerTeamArrData = reducedMatchPerTeamArrData.map(id => matchData.find(item => item.match_id === id));
-
-    return matchPerTeamArrData;
-  }
-
-  if (playerId && teamId) {
-    const matchPerPlayerAndTeamArr = statData.filter(item => item.player_id === playerId && item.team_id === teamId);
-
-    if (!matchPerPlayerAndTeamArr.length) {
-      return [];
-    }
-
-    const matchPerPlayerAndTeamIds = matchPerPlayerAndTeamArr.map(item => item.match_id);
-    const matchPerPlayerAndTeamData = matchPerPlayerAndTeamIds.map(id => matchData.find(item => item.match_id === id));
-
-    return matchPerPlayerAndTeamData;
-  }
-
-  return matchData;
-};
-
-const Filters = () => {
+const Filters = ({
+  teamData,
+  playerData,
+  matchData,
+  statData,
+  onClick,
+ }) => {
   const [teamId, setTeamId] = useState('');
   const [playerId, setPlayerId] = useState('');
   const [matchId, setMatchId] = useState('');
@@ -132,9 +27,17 @@ const Filters = () => {
     }
   };
 
-  const players = getPlayersData(teamId, matchId);
-  const teams = getTeamsData(playerId, matchId);
-  const matches = getMatchesData(teamId, playerId);
+  const handleSearchButtonClick = () => {
+    onClick({
+      teamId,
+      playerId,
+      matchId,
+    });
+  };
+
+  const players = getPlayersData(teamId, matchId, statData, playerData);
+  const teams = getTeamsData(playerId, matchId, statData, teamData);
+  const matches = getMatchesData(teamId, playerId, statData, matchData);
 
   return (
     <div className="filters">
@@ -203,6 +106,13 @@ const Filters = () => {
               </select>
             </label>
           </div>
+        </Column>
+        <Column>
+          <button
+            onClick={handleSearchButtonClick}
+          >
+            Search
+          </button>
         </Column>
       </Row>
     </div>

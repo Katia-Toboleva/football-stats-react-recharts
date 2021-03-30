a61d5cbacb174965b510c9a757d587c28fbcab4c
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-03-30 01:02:06 +0100

    chart container

--- client/app/components/chart-container/chart-container.jsx
+++ client/app/components/chart-container/chart-container.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ParentSize from '@visx/responsive/lib/components/ParentSize';
 
 import Chart from '../chart';
-import statData from '../../mocks/stat-data.json';
 
 const ChartContainer = ({ searchResult }) => {
-  const convertedData = (Object.entries(searchResult[0]).reduce((acc, currentItem) => [...acc, {
+  const convertedData = (Object.entries(searchResult).reduce((acc, currentItem) => [...acc, {
     axis: currentItem[0],
     value: currentItem[1],
   }], [])).splice(4);

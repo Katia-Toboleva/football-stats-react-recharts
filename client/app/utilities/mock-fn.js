12fc41be603199f7ffe60b2b030eeb4427284d62
Katia-Toboleva <e.toboleva@hotmail.co.uk>
2021-03-30 01:02:38 +0100

    logic for fetch fn

--- client/app/utilities/mock-fn.js
+++ client/app/utilities/mock-fn.js
@@ -1,15 +1,16 @@
-import playerData from '../mocks/player-data.json';
 import statData from '../mocks/stat-data.json';
 
-export const fetchResults = (searchValue) => {
+const getFilteredResults = ({ teamId, playerId, matchId }) => {
+  const filteredResults = statData.find(item => item.team_id === teamId && item.player_id === playerId && item.match_id === matchId);
+
+  return filteredResults;
+};
+
+export const fetchResults = (data) => {
   const delay = Math.floor(Math.random() * 3000) + 1;
   const failureRandomNumber = Math.floor(Math.random() * 10) + 1;
 
-  const searchedPlayer = playerData.find(({player_name}) => (
-    player_name.toLocaleLowerCase()).startsWith(searchValue)
-  );
-  const searchedPlayerId = searchedPlayer.player_id;
-  const filteredResultsByPlayer = statData.filter((item) => item.player_id === searchedPlayerId);
+  const results = getFilteredResults(data);
 
   return new Promise((resolve, reject) => {
     if (failureRandomNumber < 2) {
@@ -19,7 +20,7 @@ export const fetchResults = (searchValue) => {
     }
 
     window.setTimeout(() => {
-      resolve(filteredResultsByPlayer);
+      resolve(results);
     }, delay);
   });
 };
